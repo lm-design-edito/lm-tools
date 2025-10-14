b3ee6aa651c0dac7fb9154a600d94679660ccdad
Maxime Fabas <fabas@lemonde.fr>
2025-10-14 16:11:43 +0200

    /agnostic/numbers/round - added ceil and floor exports

--- src/agnostic/numbers/round/index.ts
+++ src/agnostic/numbers/round/index.ts
@@ -2,3 +2,13 @@ export function round (number: number, nbDecimals: number) {
   const multiplier = Math.pow(10, nbDecimals)
   return Math.round(number * multiplier) / multiplier
 }
+
+export function ceil (number: number, nbDecimals: number) {
+  const multiplier = Math.pow(10, nbDecimals)
+  return Math.ceil(number * multiplier) / multiplier
+}
+
+export function floor (number: number, nbDecimals: number) {
+  const multiplier = Math.pow(10, nbDecimals)
+  return Math.floor(number * multiplier) / multiplier
+}
