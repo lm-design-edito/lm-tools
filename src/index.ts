2e29140b929200712630b7df61d8984bb34fff5c
Maxime Fabas <fabas@lemonde.fr>
2025-10-30 12:09:23 +0100

    -

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Agnostic } from './agnostic'
-export { Components } from './components'
-export * as Node from './node'
+export { Agnostic } from './agnostic/index.js'
+export { Components } from './components/index.js'
+export * as Node from './node/index.js'
