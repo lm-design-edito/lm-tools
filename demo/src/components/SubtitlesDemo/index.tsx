f30b855f341b9e396cccbc1248b7b87000e634fd
itsleaht <leaztanda@gmail.com>
2026-03-04 11:24:21 +0100

    demo subtitles: add demo styles + timer

--- demo/src/components/SubtitlesDemo/index.tsx
+++ demo/src/components/SubtitlesDemo/index.tsx
@@ -1,17 +1,38 @@
-import { type FunctionComponent } from 'react'
+import { useState, useEffect, type FunctionComponent } from 'react'
 import {
   Subtitles,
   type Props as SubtitlesProps
 } from '~/components/Subtitles/index.js'
 import { CompDisplayer } from '../CompDisplayer/index.js'
+import { subtitles as publicClassName } from '~/components/public-classnames.js'
 
 const name = 'Subtitles'
 const description = 'Some description'
 const tsxDetails = `'Use wisely'`
 
+
+/* Demo CSS */
+const demoStyles = `
+.${publicClassName} {
+  position: relative;
+}
+
+.${publicClassName}__group,
+.${publicClassName}__sub {
+  opacity: 0;
+  transition: opacity 0.3s ease-in;
+}
+
+.${publicClassName}__group--curr,
+.${publicClassName}__group--prev,
+.${publicClassName}__sub--curr,
+.${publicClassName}__sub--prev {
+  opacity: 1;
+`
+
 const props1: SubtitlesProps = {
   src: 'https://assets-decodeurs.lemonde.fr/redacweb/2305-audio-quote-assets/chantal.srt',
-  timecodeMs: 29752
+  timecodeMs: 0
 }
 
 const srtFileContent = `
@@ -30,15 +51,35 @@ j'enlève`
 const props2: SubtitlesProps = {
   src: 'https://assets-decodeurs.lemonde.fr/redacweb/2305-audio-quote-assets/chantal.srt',
   srtFileContent,
-  timecodeMs: 29752
+  timecodeMs: 0
 }
 
+const demoNextTickTimecode = 500
+const demoMaxTimecode =  39210 + demoNextTickTimecode
+
 export const SubtitlesDemo: FunctionComponent = () => {
+  const [timecodeMs, setTimecodeMs] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimecodeMs(prev => {
+        const next = prev + demoNextTickTimecode
+        if (next >= demoMaxTimecode) {
+          clearInterval(interval)
+        }
+        return next
+      })
+    }, demoNextTickTimecode)
+    return () => clearInterval(interval)  
+  }, []);
+
+  console.log('timecodeMs', timecodeMs)
+
   return <CompDisplayer
     name={name}
+    demoStyles={demoStyles}
     description={description}
     tsxDetails={tsxDetails}>
-    <Subtitles {...props1} />
-    <Subtitles {...props2} />
+    <Subtitles {...props1} timecodeMs={timecodeMs} />
+    <Subtitles {...props2} timecodeMs={timecodeMs} />
   </CompDisplayer>
 }
