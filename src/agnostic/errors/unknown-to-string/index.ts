fc76bc58d54218d12ebfc1c0799c6e071edd966e
lm-maxfab <fabas@lemonde.fr>
2026-03-13 15:06:13 +0100

    agnostic/errors/unknown-to-string - handle Buffers now using buf.toString()

--- src/agnostic/errors/unknown-to-string/index.ts
+++ src/agnostic/errors/unknown-to-string/index.ts
@@ -1,13 +1,14 @@
 /**
  * Converts an unknown object into a string
  *
- * @param {unknown} err - the unknown object to stringify
+ * @param {unknown} unk - the unknown object to stringify
  * @returns {string} The stringified object
  */
-export function unknownToString (err: unknown): string {
-  if (err instanceof Error) return err.message
-  if (typeof err === 'string') return err
-  if (typeof err === 'object' && err !== null) return JSON.stringify(err)
+export function unknownToString (unk: unknown, encoding?: BufferEncoding): string {
+  if (unk instanceof Error) return unk.message
+  if (unk instanceof Buffer) return unk.toString(encoding ?? 'utf-8')
+  if (typeof unk === 'string') return unk
+  if (typeof unk === 'object' && unk !== null) return JSON.stringify(unk)
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  return `${err}`
+  return `${unk}`
 }
